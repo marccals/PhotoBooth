49c72c3c3200534f96370a9d418e9d712b6627c5
Marc Cals Perich <marcfocus@hotmail.com>
2017-06-05 21:57:27 +0200

    Added helper class to capture GPIO button input.

---
+++ InputButton.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+
+
+class InputButton:
+    """Helper class for blinking leds."""
+
+    def __init__(self, GPIO_channel):
+
+        self.__GPIO_channel = GPIO_channel
+
+        self.__setup_GPIO_channel()
+
+        return
+
+    def __del__(self):
+        """Clean up GPIO in destructor to gracefully shutdown"""
+        GPIO.cleanup()
+
+    def __setup_GPIO_channel(self):
+        """Setup GPIO channel"""
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.__GPIO_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def wait_for_input(self):
+        GPIO.wait_for_edge(self.__GPIO_channel, GPIO.FALLING)
