d04abe2f3e710af1662d8188ba62ba6745cb18e4
Marc Cals Perich <marcfocus@hotmail.com>
2017-08-03 21:21:22 +0200

    Add events to InputButton

--- InputButton.py
+++ InputButton.py
@@ -22,5 +22,12 @@ class InputButton:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__GPIO_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    def wait_for_event(self, callback_event):
+        GPIO.add_event_detect(self.__GPIO_channel, GPIO.FALLING)
+        GPIO.add_event_callback(self.__GPIO_channel, callback_event)
+
+    def cancel_wait_for_event(self):
+        GPIO.remove_event_detect(self.__GPIO_channel)
+
     def wait_for_input(self):
         GPIO.wait_for_edge(self.__GPIO_channel, GPIO.FALLING)
