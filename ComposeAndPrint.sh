35208d85856d77a563c76938ee835a4d292cf0f6
Marc Cals Perich <marcfocus@hotmail.com>
2017-08-03 21:20:50 +0200

    Add script to compose image to print and print

---
+++ ComposeAndPrint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+convert $1 -rotate 90 ./tmp/LeftPhoto.png
+convert $1 -rotate -90 ./tmp/RightPhoto.png
+
+montage ./images/LeftTextPhoto.png ./tmp/LeftPhoto.png ./images/DashLine.png ./tmp/RightPhoto.png ./images/RightTextPhoto.png -tile 5x1 -geometry +0+0 $2
+
+lp -d Canon_MX_470_series-1 $2
